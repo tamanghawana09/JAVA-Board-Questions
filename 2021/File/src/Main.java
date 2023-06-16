cf28fc5e6093554b46596337b662dedb3ee8b20e
tamanghawana09 <hawanatamang@gmail.com>
2023-06-17 00:10:57 +0545

    Code added

---
+++ 2021/File/src/Main.java
@@ -0,0 +1,20 @@
+import java.io.File;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        Scanner input = new Scanner(System.in);
+        File file = new File("Note.txt");
+        file.createNewFile();
+
+        FileWriter fileWriter = new FileWriter("Note.txt");
+        System.out.println("Enter the text: ");
+        String content = input.nextLine();
+        fileWriter.write(content);
+        fileWriter.close();
+
+    }
+}
