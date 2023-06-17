460296bb55b5c7086602470aebb7f20e71cf7b30
tamanghawana09 <hawanatamang@gmail.com>
2023-06-17 22:24:10 +0545

    Code added

---
+++ 2021/Package/src/calculate/Calculation.java
@@ -0,0 +1,8 @@
+package calculate;
+
+public class Calculation {
+    public int addNumber(int num1, int num2){
+        return num1+num2;
+    }
+
+}
---
+++ 2021/Package/src/calculate/Main.java
@@ -0,0 +1,9 @@
+package calculate;
+
+public class Main {
+    public static void main(String[] args) {
+        Calculation obj = new Calculation();
+        int result = obj.addNumber(10,20);
+        System.out.println("The added number is :" + result);
+    }
+}
